a9563723b06a310b42de1da4a2df71695a4a2980
Djavan-N <djavan@codefellows.com>
2021-02-19 19:31:17 +0000

    done correctly

--- ops4.sh
+++ ops4.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-main () {
-    file=("file1" "file2" "file3" "file4")
-    for i in "${file[@]}";
+
+
+dir=("dir1" "dir2" "dir3" "dir4")
+file=("file1" "file2" "file3" "file4")
+
+main() {
+    for k in {0..3};
     do
-        touch "$i"
+        if [ -d "$HOME/dev/${dir[$k]}" ]; then
+            echo -e "\n${dir[$k]} exists already\n"
+        else
+            mkdir "$HOME/dev/${dir[$k]}"       
+        fi  
+       
+        if [ -f "$HOME/dev/${dir[$k]}/${file[$k]}" ]; then
+            echo -e "\n${file[$k]} exists already, creating new ones\n"
+            rm "$HOME/dev/${dir[$k]}/${file[$k]}"
+        else
+            touch "$HOME/dev/${dir[$k]}/${file[$k]}"    
+        fi 
     done
 }
 
 main
-echo "I will now create the text files ${file[*]}"
